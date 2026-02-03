22a69ab78da0bef9d206daf08ce5f9f2989c8cec
santimontironi <santiimontironi@gmail.com>
2026-02-03 17:40:23 -0300

    mejora de cors

--- backend/main.js
+++ backend/main.js
@@ -20,8 +20,6 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
-app.options("*", cors())
-
 //Habilita a mi servidor a recibir json por body
 app.use(express.json())
 

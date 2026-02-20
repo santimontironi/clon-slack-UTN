ca55091937841bfec33067ef737060007f10927c
santimontironi <santiimontironi@gmail.com>
2026-02-19 22:22:50 -0300

    mejora en HeaderChannel

--- frontend/src/components/channelComponents/HeaderChannel.jsx
+++ frontend/src/components/channelComponents/HeaderChannel.jsx
@@ -1,7 +1,7 @@
 const HeaderChannel = ({ channelName }) => {
     return (
         <header className="bg-[#3F0E40] border-b border-[#522653] px-6 py-3">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center ml-10 mt-2 md:ml-0 md:mt-0 gap-2">
                 <span className="text-gray-400 text-xl">#</span>
                 <h2 className="text-white font-bold text-lg">{channelName}</h2>
             </div>
